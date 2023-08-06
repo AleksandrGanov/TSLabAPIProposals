90b19dd13c9c3bf56d40258d39a7193999db6053
Aleksandr Ganov <30367100+AleksandrGanov@users.noreply.github.com>
2023-08-06 14:57:30 +1200

    Update API: работа со скриптами.cs

--- API: работа со скриптами.cs
+++ API: работа со скриптами.cs
@@ -1,15 +1,15 @@
 // открывает указанный скрипт 
 // scriptName - имя скрипта
 // bool - возвращает True если скрипт найден, False - если скрипт найти не удалось
-bool Gontext.OpenScript(string scriptName)
+bool Сontext.OpenScript(string scriptName)
 
 // закрывает указанный скрипт
 // scriptName - имя скрипта
 // bool - возвращает True если скрипт найден, False - если скрипт найти не удалось
-bool Gontext.CloseScript(string scriptName)
+bool Сontext.CloseScript(string scriptName)
 
 // закрывает все открытые скрипты
-bool Gontext.CloseAllScripts
+bool Сontext.CloseAllScripts
 
 // возвращает коллекцию имен закрытых скриптов
 IEnumerable<string> GetAllOpenedScripts()
