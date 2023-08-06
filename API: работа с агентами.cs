0c73d7f2f1ed9a981660d0219c0eb7fd31b1959c
Aleksandr Ganov <30367100+AleksandrGanov@users.noreply.github.com>
2023-08-06 23:31:59 +1200

    Update API: работа с агентами.cs

--- API: работа с агентами.cs
+++ API: работа с агентами.cs
@@ -1,6 +1,8 @@
 // создает агента с указанными именем, источником, инструментами
-// bool - возвращает True если агент успешно создан (не произошло ошибки создания)
-bool Сontext.CreateAgent(string agentName, IEnumerable<(source, sec)>){}
+// bool - возвращает True если агент успешно создан, то есть не произошло ошибки создания.
+// Под ошибкой создания подразумеваются любые ошибки начиная от неверно заданного имени имени скрипта
+// на основании которого создается агент, заканчивая неверно переданными в метод именами источника/бумаги
+bool Сontext.CreateAgent(string agentName, string scriptName, IEnumerable<(source, sec)>){}
 
 // удаляет агента с указанными именем
 // bool - возвращает True если агент успешно удален (не произошло ошибки создания)
@@ -25,21 +27,25 @@ bool Сontext.StopAllAgents(){}
 // возвращает коллекцию имен и статусов агентов
 IEnumerable<(string scriptName, AgentStatus agentStatus)> Context.GetAgentsInfo(){}
 
-// метод выдает коллекцию имен торговых параметров агента (торговые 
-IEnumerable<(string tradeParam, object paramValue)> Context.GetAgentTradeParams(){}
+// метод выдает коллекцию имен торговых параметров агента, в случае неверно указанного имени
+// агента выдает пустую коллекцию
+IEnumerable<(string tradeParam, object paramValue)> Context.GetAgentTradeParams(string agentName){}
 
-// метод выдает коллекцию имен торговых параметров агента (торговые 
-IEnumerable<(string tradeParam, object paramValue)> Context.GetAgentParams(){}
+// метод выдает коллекцию имен торговых параметров агента, в случае неверно указанного имени
+// агента выдает пустую коллекцию
+IEnumerable<(string tradeParam, object paramValue)> Context.GetAgentParams(string agentName){}
 
 // метод устанавливает торговые параметры агента
 // bool - возвращает True только в случае если все параметры удалось установить
-// False - возвращется при наличии любых ошибок
-bool Context.SetAgentTradeParams(IEnumerable<(string paramName, object paramValue)>)
+// False - возвращется при наличии любых ошибок от попытка задать параметры агента
+// в статусах: Running, Error, заканчивая неверное переданным именем агента, именами/типами параметров
+bool Context.SetAgentTradeParams(string agentName, IEnumerable<(string paramName, object paramValue)>)
 
 // метод устанавливает параметры агента
 // bool - возвращает True только в случае если все параметры удалось установить
-// False - возвращется при наличии любых ошибок
-bool Context.SetAgentParams(IEnumerable<(string paramName, object paramValue)>)
+// False - возвращется при наличии любых ошибок от попытка задать параметры агента
+// в статусах: Running, Error, заканчивая неверное переданным именем агента, именами/типами параметров
+bool Context.SetAgentParams(string agentName, IEnumerable<(string paramName, object paramValue)>)
 
 // все задачи по определению типов значений параметров ложатся на пользователя, чтобы не усложнять API
 
